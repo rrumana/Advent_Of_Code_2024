57f41b8621a2cf5c45a915b106df7707043ed856
rrumana <ryan.rumana@gmail.com>
2024-12-03 07:19:18 -0800

    Added day 3 solution

--- day_3/src/main.rs
+++ day_3/src/main.rs
@@ -1,13 +1,53 @@
 use anyhow::Result;
+use regex::Regex;
+use std::fs;
 
 // The most basic template for this project.
 
-fn part_one(_filepath: &str) -> Result<i32> {
-    Ok(0)
+fn prep_data(filepath: &str) -> Result<String> {
+    let input: String = fs::read_to_string(filepath)?;
+    Ok(input)
 }
 
-fn part_two(_filepath: &str) -> Result<i32> {
-    Ok(0)
+// part one functions
+
+fn get_sum(input: &str) -> Result<i32> {
+    let re = match Regex::new(r"mul\((\d+),(\d+)\)") {
+        Ok(re) => re,
+        Err(e) => return Err(anyhow::anyhow!("Could not create regex: {}", e)),
+    };
+
+    let mut sum = 0;
+
+    for (_, [first, second]) in re.captures_iter(input).map(|c| c.extract()) {
+        let result = first.parse::<i32>()? * second.parse::<i32>()?;
+        sum += result;
+    }
+
+    Ok(sum)
+}
+
+fn part_one(filepath: &str) -> Result<i32> {
+    let data = prep_data(filepath)?;
+    let ans = get_sum(&data)?;
+    
+    Ok(ans)
+}
+
+// part two functions
+
+fn part_two(filepath: &str) -> Result<i32> {
+    let data = prep_data(filepath)?;
+
+    let re = match Regex::new(r"don't\(\)[\s\S]*?do\(\)") {
+        Ok(re) => re,
+        Err(e) => return Err(anyhow::anyhow!("Could not create regex: {}", e)),
+    };
+
+    let data = re.replace_all(&data, "");
+    let ans = get_sum(&data)?;
+
+    Ok(ans)
 }
 
 fn main() {
@@ -37,8 +77,8 @@ mod tests {
 
     #[test]
     fn test_part_one() {
-        let _filepath = "part_one_test_input.txt";
-        assert!(true); // This is a placeholder
+        let filepath = "part_one_test_input.txt";
+        assert_eq!(part_one(filepath).unwrap(), 2652); // This is a placeholder
     }
 
     #[test]
